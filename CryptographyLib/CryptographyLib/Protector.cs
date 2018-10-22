9e7c57beb3cc8e66c5ca52d7bdd1fb37f9e2e5f3
Manuel Bautista <b0hcoder@gmail.com>
2018-10-22 17:59:14 -0400

    DotnetTools

---
+++ CryptographyLib/CryptographyLib/Protector.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Xml.Linq;
+using System.Security.Principal;
+
+namespace CryptographyLib
+{
+    public class Protector
+    {
+        // salt size must be at least 8 bytes, we will use 16 bytes
+        private static readonly byte[] salt = Encoding.Unicode.GetBytes("7BANANAS");
+        // iterations must be at least 1000, we will use 2000
+        private static readonly int iterations = 2000;
+        /// <summary>
+        /// Encrypt using AES
+        /// </summary>
+        /// <param name="plainText"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public static string Encrypt(string plainText, string password)
+        {
+            byte[] plainBytes = Encoding.Unicode.GetBytes(plainText);
+            var aes = Aes.Create();
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt,
+                iterations);
+            aes.Key = pbkdf2.GetBytes(32); // set a 256-bit key
+            aes.IV = pbkdf2.GetBytes(16); // set a 128-bit IV
+            var ms = new MemoryStream();
+            using (var cs = new CryptoStream(ms, aes.CreateEncryptor(),
+                CryptoStreamMode.Write))
+            {
+                cs.Write(plainBytes, 0, plainBytes.Length);
+            }
+            return Convert.ToBase64String(ms.ToArray());
+        }
+        /// <summary>
+        /// Decrypt using AES
+        /// </summary>
+        /// <param name="cryptoText"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public static string Decrypt(string cryptoText,string password)
+        {
+            byte[] cryptoBytes = Convert.FromBase64String(cryptoText);
+            var aes = Aes.Create();
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt,
+                iterations);
+            aes.Key = pbkdf2.GetBytes(32);
+            aes.IV = pbkdf2.GetBytes(16);
+            var ms = new MemoryStream();
+            using (var cs = new CryptoStream(ms, aes.CreateDecryptor(),
+                CryptoStreamMode.Write))
+            {
+                cs.Write(cryptoBytes, 0, cryptoBytes.Length);
+            }
+            return Encoding.Unicode.GetString(ms.ToArray());
+        }
+
+        private static Dictionary<string, User> Users = new Dictionary<string,
+            User>();
+        /// <summary>
+        /// Register user using SHA256 (Best Method)
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public static User Register(string username, string password, string[] roles = null)
+        {
+            // generate a random salt
+            var rng = RandomNumberGenerator.Create();
+            var saltBytes = new byte[16];
+            rng.GetBytes(saltBytes);
+            var saltText = Convert.ToBase64String(saltBytes);
+            // generate the salted and hashed password
+            var sha = SHA256.Create();
+            var saltedPassword = password + saltText;
+            var saltedhashedPassword = Convert.ToBase64String(
+                sha.ComputeHash(Encoding.Unicode.GetBytes(saltedPassword)));
+            var user = new User
+            {
+                Name = username,
+                Salt = saltText,
+                SaltedHashedPassword = saltedhashedPassword,
+                Roles = roles
+            };
+            Users.Add(user.Name, user);
+            return user;
+        }
+
+        /// <summary>
+        /// Validate User Using SHA256 (Best Method)
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public static bool CheckPassword(string username, string password)
+        {
+            if (!Users.ContainsKey(username))
+            {
+                return false;
+            }
+            var user = Users[username];
+            // re-generate the salted and hashed password
+            var sha = SHA256.Create();
+            var saltedPassword = password + user.Salt;
+            var saltedhashedPassword = Convert.ToBase64String(
+                sha.ComputeHash(Encoding.Unicode.GetBytes(saltedPassword)));
+            return (saltedhashedPassword == user.SaltedHashedPassword);
+        }
+
+        public static void RegisterSomeUsers()
+        {
+            Register("Alice", "Pa$$w0rd", new[] { "Admins" });
+            Register("Bob", "Pa$$w0rd", new[] { "Sales", "TeamLeads" });
+            Register("Eve", "Pa$$w0rd");
+        }
+
+        public static void LogIn(string username, string password)
+        {
+            if (CheckPassword(username, password))
+            {
+                var identity = new GenericIdentity(username, "PacktAuth");
+                var principal = new GenericPrincipal(identity,
+                    Users[username].Roles);
+                System.Threading.Thread.CurrentPrincipal = principal;
+            }
+        }
+
+    }
+}
---
+++ EncryptionApp/EncryptionApp/Program.cs
@@ -0,0 +1,101 @@
+﻿using static System.Console;
+using CryptographyLib;
+using System.Threading;
+using System.Security;
+using System.Security.Permissions;
+using System.Security.Principal;
+using System.Security.Claims;
+
+namespace EncryptionApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //-----------------------------------------------------------------------------------
+
+            //Write("Enter a message that you want to encrypt: ");
+            //string message = "xxxxxx"; //ReadLine();
+            //Write("Enter a password: ");
+            //string password = ReadLine();
+            //string cryptoText = Protector.Encrypt(message, password);
+            //WriteLine($"Encrypted text: {cryptoText}");
+            //Write("Enter the password: ");
+            //string password2 = ReadLine();
+            //try
+            //{
+            //    string clearText = Protector.Decrypt(cryptoText, password2);
+            //    WriteLine($"Decrypted text: {clearText}");
+            //}
+            //catch
+            //{
+            //    WriteLine(
+            //        "Enable to decrypt because you entered the wrong password!");
+            //}
+            //-----------------------------------------------------------------------------------
+
+            //WriteLine("A user named Alice has been registered with Pa$$w0rd as herpassword.");
+            //var alice = Protector.Register("Alice", "Pa$$w0rd");
+            //WriteLine($"Name: {alice.Name}");
+            //WriteLine($"Salt: {alice.Salt}");
+            //WriteLine($"Salted and hashed password: {alice.SaltedHashedPassword}");
+            //WriteLine();
+            //Write("Enter a different username to register: ");
+            //string username = ReadLine();
+            //Write("Enter a password to register: ");
+            //string password = ReadLine();
+            //var user = Protector.Register(username, password);
+            //WriteLine($"Name: {user.Name}");
+            //WriteLine($"Salt: {user.Salt}");
+            //WriteLine($"Salted and hashed password: {user.SaltedHashedPassword}");
+            //bool correctPassword = false;
+            //while (!correctPassword)
+            //{
+            //    Write("Enter a username to log in: ");
+            //    string loginUsername = ReadLine();
+            //    Write("Enter a password to log in: ");
+            //    string loginPassword = ReadLine();
+            //    correctPassword = Protector.CheckPassword(loginUsername,
+            //        loginPassword);
+            //    if (correctPassword)
+            //    {
+            //        WriteLine($"Correct! {loginUsername} has been logged in.");
+            //    }
+            //    else
+            //    {
+            //        WriteLine("Invalid username or password. Try again.");
+            //    }
+            //}
+
+            //-----------------------------------------------------------------------------------
+
+            Protector.RegisterSomeUsers();
+
+            Write($"Enter your user name: ");
+            string username = ReadLine();
+            Write($"Enter your password: ");
+            string password = ReadLine();
+            Protector.LogIn(username, password);
+            if (Thread.CurrentPrincipal == null)
+            {
+                WriteLine("Log in failed.");
+                return;
+            }
+            var p = Thread.CurrentPrincipal;
+            WriteLine($"IsAuthenticated: {p.Identity.IsAuthenticated}");
+            WriteLine($"AuthenticationType: {p.Identity.AuthenticationType}");
+            WriteLine($"Name: {p.Identity.Name}");
+            WriteLine($"IsInRole(\"Admins\"): {p.IsInRole("Admins")}");
+            WriteLine($"IsInRole(\"Sales\"): {p.IsInRole("Sales")}");
+            if (p is ClaimsPrincipal)
+            {
+                WriteLine($"{p.Identity.Name} has the following claims:");
+                foreach (Claim claim in (p as ClaimsPrincipal).Claims)
+                {
+                    WriteLine($" {claim.Type}: {claim.Value}");
+                }
+            }
+            ReadKey();
+        }
+    }
+}
